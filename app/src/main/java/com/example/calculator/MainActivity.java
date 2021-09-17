c9be84a3e5ca279948e731a40435474e45d39ea0
rafidenandra <rafiden01@gmail.com>
2021-09-17 15:05:42 +0700

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,193 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView workingsTV;
+    TextView resultsTV;
+    String workings = "";
+    boolean leftBracket = true;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initTextViews();
+    }
+
+    private void initTextViews() {
+        workingsTV = findViewById(R.id.workingsTextView);
+        resultsTV = findViewById(R.id.resultTextView);
+    }
+
+    private void setWorkings(String givenValue) {
+        workings = workings + givenValue;
+        workingsTV.setText(workings);
+    }
+
+    public static double calculate(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm();
+                    else if (eat('-')) x -= parseTerm();
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor();
+                    else if (eat('/')) x /= parseFactor();
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                double x;
+                int startPos = this.pos;
+
+                if (eat('+')) {
+                    return parseFactor();
+                }
+
+                if (eat('-')) {
+                    return -parseFactor();
+                }
+
+                if (eat('(')) {
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                }
+                else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) {
+                    x = Math.pow(x, parseFactor());
+                }
+
+                return x;
+            }
+        }.parse();
+    }
+
+    public void equalsOnClick(View view) {
+        String val = workingsTV.getText().toString();
+        String replacestr = val.replace('÷','/').replace('×','*');
+        double result = calculate(replacestr);
+        resultsTV.setText(String.valueOf(result));
+    }
+
+    public void clearOnClick(View view) {
+        workingsTV.setText("");
+        workings = "";
+        resultsTV.setText("");
+        leftBracket = true;
+    }
+
+    public void bracketsOnClick(View view) {
+        if (leftBracket) {
+            setWorkings("(");
+            leftBracket = false;
+        } else {
+            setWorkings(")");
+            leftBracket = true;
+        }
+    }
+
+    public void powerOfOnClick(View view) {
+        setWorkings("^");
+    }
+
+    public void divisionOnClick(View view) {
+        setWorkings("/");
+    }
+
+    public void sevenOnClick(View view) {
+        setWorkings("7");
+    }
+
+    public void eightOnClick(View view) {
+        setWorkings("8");
+    }
+
+    public void nineOnClick(View view) {
+        setWorkings("9");
+    }
+
+    public void timesOnClick(View view) {
+        setWorkings("*");
+    }
+
+    public void fourOnClick(View view) {
+        setWorkings("4");
+    }
+
+    public void fiveOnClick(View view) {
+        setWorkings("5");
+    }
+
+    public void sixOnClick(View view) {
+        setWorkings("6");
+    }
+
+    public void minusOnClick(View view) {
+        setWorkings("-");
+    }
+
+    public void oneOnClick(View view) {
+        setWorkings("1");
+    }
+
+    public void twoOnClick(View view) {
+        setWorkings("2");
+    }
+
+    public void threeOnClick(View view) {
+        setWorkings("3");
+    }
+
+    public void plusOnClick(View view) {
+        setWorkings("+");
+    }
+
+    public void decimalOnClick(View view) {
+        setWorkings(".");
+    }
+
+    public void zeroOnClick(View view) {
+        setWorkings("0");
+    }
+}
